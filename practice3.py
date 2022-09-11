d153d2f226036c737236fbca50394c56928b8046
daraDiana18 <96611331+daraDiana18@users.noreply.github.com>
2022-09-11 19:58:03 +0600

    Add files via upload

---
+++ practice3.py
@@ -0,0 +1,21 @@
+a = input("a: ")
+b = input("b: ")
+c = input("c: ")
+
+if a>b:
+    if a>c:
+        max=a
+    else:
+          max=c
+
+else:
+    if b>c:
+        max=b
+    else:
+        max=c
+
+
+
+print('max=', max)
+
+#since the task is not written to the end, I decided to find the maximum number of 3 numbers
---
+++ practice4.py
@@ -0,0 +1,12 @@
+n = int(input('enter the price of 1 kg sweets'))
+i=1
+for i in range(1,11):
+     i += 0
+
+     product = n * i
+     print('price of ', i, 'kg sweets: ', product)
+
+
+
+
+
